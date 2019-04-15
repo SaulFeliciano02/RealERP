3a64498e9a2af359a2666e9f4f7d34218c01b0a3
marco <20160370@ce.pucmm.edu.do>
2019-04-15 19:40:38 -0400

    Creacion de clases de costoIndirecto y moneda, pequeña modificacion de moneda

---
+++ ERP/src/logico/CostoIndirecto.java
@@ -0,0 +1,24 @@
+package logico;
+
+public class CostoIndirecto {
+	
+	private String Nombre;
+	
+	
+
+	public CostoIndirecto(String nombre) {
+		super();
+		this.Nombre = nombre;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	
+	
+
+}
