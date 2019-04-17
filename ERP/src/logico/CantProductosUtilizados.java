86452165ec5977257edc5fdb810fd9eff91fdcfb
SaulFeliciciano <bobxmugiwara@gmail.com>
2019-04-17 00:20:53 -0400

    Agregando lo relacionado con partida y producto estandar

---
+++ ERP/src/logico/CantProductosUtilizados.java
@@ -0,0 +1,30 @@
+package logico;
+
+public class CantProductosUtilizados {
+	
+	public Producto producto;
+	public float cantidad;
+	
+	public CantProductosUtilizados(Producto producto, float cantidad) {
+		super();
+		this.producto = producto;
+		this.cantidad = cantidad;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public float getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(float cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+}
---
+++ ERP/src/logico/Combinaciones.java
@@ -0,0 +1,57 @@
+package logico;
+
+import java.util.ArrayList;
+
+public class Combinaciones {
+	
+	public ArrayList<Atributos> listaAtributos;
+	public String numeroSerie;
+	public float existenciaActual;
+	public Partida partida;
+	
+	public Combinaciones(String numeroSerie, float existenciaActual,
+			Partida partida) {
+		super();
+		this.listaAtributos = new ArrayList<>();
+		this.numeroSerie = numeroSerie;
+		this.existenciaActual = existenciaActual;
+		this.partida = partida;
+	}
+
+	public ArrayList<Atributos> getListaAtributos() {
+		return listaAtributos;
+	}
+
+	public String getNumeroSerie() {
+		return numeroSerie;
+	}
+
+	public void setNumeroSerie(String numeroSerie) {
+		this.numeroSerie = numeroSerie;
+	}
+
+	public Partida getPartida() {
+		return partida;
+	}
+
+	public void setPartida(Partida partida) {
+		this.partida = partida;
+	}
+
+	public float getExistenciaActual() {
+		return existenciaActual;
+	}
+	
+	public void disminuirExistenciaActual(float reduc) {
+		this.existenciaActual -= reduc; 
+	}
+	
+	public void agregarALaExistencia(float sum) {
+		this.existenciaActual += sum;
+	}
+	
+	public void agregarAtributo(Atributos atributo) {
+		this.listaAtributos.add(atributo);
+	}
+	
+}
