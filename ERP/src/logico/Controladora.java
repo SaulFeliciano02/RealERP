b60b085f4c875950a4eb8de83421664fee233fb6
marco <20160370@ce.pucmm.edu.do>
2019-04-19 13:52:52 -0400

    Añadido buscadores.

--- ERP/src/logico/Controladora.java
+++ ERP/src/logico/Controladora.java
@@ -77,6 +77,135 @@ public class Controladora implements Serializable{
 		return misProductos;
 	}
 	
+	/**FUNCION PARA BUSCAR UN PRODUCTO**/
+	
+	public ArrayList<Producto> searchProducts(String buscador, String tipoBusqueda) {
+		ArrayList<Producto> searchProducto = new ArrayList<>();
+		for(int i = 0; i < misProductos.size(); i++) {
+			int boolCount = 0;
+			/**Con motivo de no repetir la misma funcion varias veces, se penso hacerlo un switch**/
+			for(int j = 0; j < buscador.length(); j++) {
+				switch(tipoBusqueda) {
+				case "Codigo":
+					if(misProductos.get(i).getCodigo().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				case "Nombre":
+					if(misProductos.get(i).getNombre().toLowerCase().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				case "Descripcion":
+					if(misProductos.get(i).getDescripcion().toLowerCase().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				case "Proveedor":
+					if(misProductos.get(i).getProveedorPrin().getCodigo().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				case "Rubro":
+					if(misProductos.get(i).getRubroProducto().getNombreRubro().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				}
+				
+			}
+			if(boolCount == buscador.length()) {
+				searchProducto.add(misProductos.get(i));
+			}
+		}
+		return searchProducto;
+	}
 	
 
+/**FUNCION PARA BUSCAR PROVEEDOR**/
+	
+	public ArrayList<Proveedores> searchProveedores(String buscador, String tipoBusqueda){
+		ArrayList<Proveedores> searchProveedor = new ArrayList<>();
+		for(int i = 0; i < misProveedores.size(); i++) {
+			int boolCount = 0;
+			for(int j = 0; j < tipoBusqueda.length(); j++) {
+				switch(tipoBusqueda) {
+				case "Codigo":
+					if(misProveedores.get(i).getCodigo().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				
+				case "Nombre":
+					if(misProveedores.get(i).getNombre().toLowerCase().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				case "Rubro":
+					if(misProveedores.get(i).getRubro().getNombreRubro().toLowerCase().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				}
+			}
+			if(boolCount == buscador.length()) {
+				searchProveedor.add(misProveedores.get(i));
+			}
+		}
+		return searchProveedor;
+	}
+	
+/**FUNCION PARA BUSCAR CLIENTES**/
+	
+	public ArrayList<Cliente> searchClientes(String buscador, String tipoBusqueda){
+		ArrayList<Cliente> searchCliente = new ArrayList<>();
+		for(int i = 0; i < misClientes.size(); i++) {
+			int boolCount = 0;
+			for(int j = 0; j < buscador.length(); j++) {
+				switch(tipoBusqueda) {
+				case "Codigo":
+					if(misClientes.get(i).getCodigo().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				case "Nombre":
+					if(misClientes.get(i).getNombre().toLowerCase().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				}
+			}
+			if(boolCount == buscador.length()) {
+				searchCliente.add(misClientes.get(i));
+			}
+		}
+		return searchCliente;
+	}
+	
+/**FUNCION PARA BUSCAR EMPLEADOS**/
+	
+	public ArrayList<Empleado> searchEmpleados(String buscador, String tipoBusqueda){
+		ArrayList<Empleado> searchEmpleado = new ArrayList<>();
+		for(int i = 0; i < misEmpleados.size(); i++) {
+			int boolCount = 0;
+			for(int j = 0; j < buscador.length(); j++) {
+				switch(tipoBusqueda) {
+				case "Codigo":
+					if(misEmpleados.get(i).getCodigo().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				case "Nombre":
+					if(misEmpleados.get(i).getNombre().toLowerCase().charAt(j) == buscador.charAt(j)) {
+						boolCount++;
+					}
+					break;
+				}
+			}
+			if(boolCount == buscador.length()) {
+				searchEmpleado.add(misEmpleados.get(i));
+			}
+		}
+		return searchEmpleado;
+	}
 }
