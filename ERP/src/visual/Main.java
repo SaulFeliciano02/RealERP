f65e6efb2c971b34144b50fbc1982d183765815a
Yorman <yormanrdrgz@gmail.com>
2019-08-21 03:10:06 -0400

    Peticiones de productos

--- ERP/src/visual/Main.java
+++ ERP/src/visual/Main.java
@@ -194,6 +194,129 @@ public class Main extends Application{
 		passwordfield_login.clear();
 		
 	}
+	
+	@FXML
+	public void onEnter(ActionEvent event){
+		float count=0;
+		if (!passwordfield_login.getText().equals("")) {
+			message.setText("¡Tu contraseña es incorrecta!");
+			message.setTextFill(Color.rgb(210, 39, 30));
+			
+			loading_progress.setVisible(false);
+			loading_progress.setProgress(0);
+			
+		} else {
+			
+	    	/**ABRIENDO viewPrincipal.fxml**/
+			message.setText("Tu contraseña ha sido confirmada");
+			message.setTextFill(Color.rgb(21, 117, 84));
+			
+			loading_progress.setVisible(true);
+			
+			Timeline timeline = new Timeline();
+
+			KeyValue keyValue = new KeyValue(loading_progress.progressProperty(), 1);
+			KeyFrame keyFrame = new KeyFrame(new Duration(1000), keyValue);
+			timeline.getKeyFrames().add(keyFrame);
+
+			timeline.play();
+			Stage loginStage = (Stage) button_acceder.getScene().getWindow();
+			loginStage.close();
+				try {
+					//Controladora.getInstance().sendUnidadesIntoDatabase();
+					//Controladora.getInstance().loadProductos();
+					if(Controladora.getInstance().activarLoadGastosGenerales())
+					{
+						Controladora.getInstance().loadGastosGenerales();
+					}
+					if(Controladora.getInstance().activarRecuperarRubros())
+					{
+						Controladora.getInstance().recuperarRubros();
+					}
+					if(Controladora.getInstance().activarLoadProveedores())
+					{
+						Controladora.getInstance().loadProveedores();
+					}
+					if(Controladora.getInstance().activarLoadCategoriaEmpleado())
+					{
+						Controladora.getInstance().loadCategoriaEmpleado();
+					}
+					if(Controladora.getInstance().activarLoadEmpleados())
+					{
+						Controladora.getInstance().loadEmpleados();
+					}
+					if(Controladora.getInstance().activarLoadPrecio())
+					{
+						Controladora.getInstance().loadPrecio();
+					}
+					if(Controladora.getInstance().activarLoadProductos())
+					{
+						Controladora.getInstance().loadProductos();
+					}
+					if(Controladora.getInstance().activarLoadPartida())
+					{
+						Controladora.getInstance().loadPartida();
+					}
+					if(Controladora.getInstance().activarLoadGrupoAtributo())
+					{
+						Controladora.getInstance().loadGrupoAtributo();
+					}
+					if(Controladora.getInstance().activarLoadAtributos())
+					{
+						Controladora.getInstance().loadAtributos();
+					}
+					if(Controladora.getInstance().activarloadMatriz())
+					{
+						Controladora.getInstance().loadMatriz();
+					}
+					if(Controladora.getInstance().activarLoadKit())
+					{
+						Controladora.getInstance().loadKit();
+					}
+					if(Controladora.getInstance().activarLoadServicios())
+					{
+						Controladora.getInstance().loadServicios();
+					}
+					if(Controladora.getInstance().activarLoadManoDeObra())
+					{
+						Controladora.getInstance().loadManoDeObra();
+					}
+					if(Controladora.getInstance().activarLoadCostoIndirecto())
+					{
+						Controladora.getInstance().loadCostoIndirecto();
+					}
+					if(Controladora.getInstance().activarLoadPromedioGananciaAnual())
+					{
+						Controladora.getInstance().loadPromedioGananciaAnual();
+					}
+					Controladora.getInstance().loadFactura();
+					Controladora.getInstance().loadPromocion();
+					if(Controladora.getInstance().activarLoadInfoEmpresa())
+					{
+						Controladora.getInstance().loadInfoEmpresa();
+					}
+					if(Controladora.getInstance().activarLoadImagenProducto())
+					{
+						Controladora.getInstance().loadImagenProducto();
+					}
+						FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("viewPrincipal.fxml"));
+						Parent root1;
+						root1 = (Parent) fxmlLoader.load();
+						Stage stage = new Stage();
+						stage.setTitle("Centro Pymes");
+						stage.setScene(new Scene(root1)); 
+						stage.getIcons().add(new Image(Main.class.getResourceAsStream("images/favicon.png")));
+						stage.setMaximized(true);
+
+						stage.showAndWait();
+						
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+		}
+		passwordfield_login.clear();
+	}
 		
 		
 	public void handleCloseButtonAction(ActionEvent event) {
